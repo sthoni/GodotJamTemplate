085750277a68b78bd38b0e048f7532ed1ae5b9d0
sthoni <sebastian-thon@web.de>
2025-12-21 01:20:44 +0100

    Font und State hinzugefügt

--- components/state.gd
+++ components/state.gd
@@ -1,1 +1,7 @@
-extends Node
+@abstract
+class_name State extends Node
+
+@abstract func enter() -> void
+@abstract func exit() -> void
+@abstract func update(_delta: float) -> void
+@abstract func physics_update(_delta: float) -> void
